7d45644376932cd9e8b65daf8ecc2be3c40f05aa
Evan  Markert <evanmarkert@gmail.com>
2017-12-05 14:10:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,7 +17,15 @@ end
 
 
 def sort_array_char_count(array)
-
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
 end
 
 
